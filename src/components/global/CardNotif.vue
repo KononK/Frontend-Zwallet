4716f6e2d6577a192d150e3d231fa2fba18055d1
AgungWicaksonoPutro <agungwicaksonoputro.dev@gmail.com>
2020-09-29 11:39:52 +0700

    Notif on global component

---
+++ src/components/global/CardNotif.vue
@@ -0,0 +1,47 @@
+<template>
+<b-row class="no-gutters cards p-2 my-2">
+    <b-col lg="1">
+        <div class="icon px-2">
+        <b-icon icon="arrow-up"></b-icon>
+        </div>
+    </b-col>
+    <b-col lg="11">
+        <b-row class="no-gutters mx-2 title">Netflix subscription</b-row>
+        <b-row class="no-gutters mx-2 amount my-2">Rp149.000</b-row>
+    </b-col>
+</b-row>
+</template>
+
+<script>
+export default {
+  name: 'CardNotif'
+}
+</script>
+
+<style scoped>
+.cards{
+    background: #FFFFFF;
+    box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.05);
+    border-radius: 10px;
+}
+.icon{
+    color: #EE5A38;
+    font-size: 20px;
+}
+.title{
+    font-style: normal;
+    font-weight: normal;
+    font-size: 14px;
+    line-height: 19px;
+    color: #7A7A7A;
+    letter-spacing: 1px;
+}
+.amount{
+    font-style: normal;
+    font-weight: bold;
+    font-size: 18px;
+    line-height: 25px;
+    color: #43484F;
+    letter-spacing: 1px;
+}
+</style>
--- src/components/global/UserAccaount.vue
+++ src/components/global/UserAccaount.vue
@@ -10,14 +10,50 @@
             <b-row><div class="phone">+62 567890</div></b-row>
         </b-col>
         <b-col lg="2">
-            <b-icon icon="bell"></b-icon>
+            <b-icon class="bell" icon="bell" @click="handleNotif"></b-icon>
+            <transition name="dropdown">
+             <div class="dropdown__menu" v-bind:class="{ active: show }" v-show="show">
+                 <b-row class="no-gutters mx-3">
+                     <b-col lg="12">
+                         <div class="title">Today</div>
+                     </b-col>
+                     <b-col lg="12 mt-3 mb-2">
+                         <CardNotif/>
+                         <CardNotif/>
+                     </b-col>
+                 </b-row>
+                 <b-row class="no-gutters mx-3">
+                     <b-col lg="12">
+                         <div class="title">This Week</div>
+                     </b-col>
+                     <b-col lg="12 mt-3 mb-2">
+                         <CardNotif/>
+                         <CardNotif/>
+                     </b-col>
+                 </b-row>
+             </div>
+         </transition>
         </b-col>
     </b-row>
 </template>
 
 <script>
+import CardNotif from '../global/CardNotif'
 export default {
-  name: 'UserAccount'
+  name: 'UserAccount',
+  data () {
+    return {
+      show: false
+    }
+  },
+  components: {
+    CardNotif
+  },
+  methods: {
+    handleNotif () {
+      this.show = !this.show
+    }
+  }
 }
 </script>
 
@@ -50,5 +86,53 @@ export default {
     font-size: 13px;
     line-height: 24px;
     color: rgba(58, 61, 66, 0.9);
+}
+.bell{
+    cursor: pointer;
+}
+.dropdown__menu {
+     top: 100%;
+     right: 0.5rem;
+     position: absolute;
+     z-index: 10;
+     height: auto;
+     min-width: 400px;
+     margin-top: 2.1rem;
+     overflow-y: auto;
+     padding: 1rem 1rem 1rem 0rem;
+     background: #FFFFFF;
+     box-shadow: 0px 0px 12px rgba(99, 99, 99, 0.15);
+     border-radius: 20px;
+}
+.dropdown__menu-nav{
+    list-style: none;
+    margin: 0;
+    padding: 10px;
+}
+.slide-fade-enter-active, .slide-fade-leave-active {
+     transition: all 0.6s;
+}
+.slide-fade-enter, .slide-fade-leave-active {
+     opacity: 0;
+}
+.slide-fade-enter {
+     transform: translateX(31px);
+}
+.slide-fade-leave-active {
+     transform: translateX(-31px);
+}
+.dropdown-enter-active, .dropdown-leave-active {
+     transition: all 1s;
+}
+.dropdown-enter, .dropdown-leave-to {
+     opacity: 0;
+     transform: translateY(30px);
+}
+.title{
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 27px;
+    color: #7A7886;
 }
 </style>
